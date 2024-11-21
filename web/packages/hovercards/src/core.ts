c8225c7659e70806353180005b50a2a7a7c77a4a
Joao Heringer <joao.heringer@automattic.com>
2024-11-21 18:10:42 -0300

    Adjust verified icon

--- web/packages/hovercards/src/core.ts
+++ web/packages/hovercards/src/core.ts
@@ -365,7 +365,7 @@ export default class Hovercards {
 					${ description ? `<p class="gravatar-hovercard__description">${ escHtml( description ) }</p>` : '' }
 				</div>
 				<div class="gravatar-hovercard__social-links">
-					<img class="gravatar-hovercard__verified-icon" src="https://secure.gravatar.com/icons/verified.svg" width="24" height="24" title="Verified accounts" alt="Verified icon">
+					<img class="gravatar-hovercard__verified-icon" src="https://secure.gravatar.com/icons/verified.svg" width="32" height="32" title="Verified accounts" alt="Verified icon">
 					<a class="gravatar-hovercard__social-link" href="${ trackedProfileUrl }" target="_blank" data-service-name="gravatar">
 						<img class="gravatar-hovercard__social-icon" src="https://secure.gravatar.com/icons/gravatar.svg" width="32" height="32" alt="Gravatar" />
 					</a>
